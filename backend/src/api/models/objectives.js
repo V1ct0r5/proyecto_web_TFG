db199e0d06cc28fbbee873a26a55f8764c2647a7
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-04-20 13:21:14 +0200

    Corrección de ruta de archivo database.js

--- backend/src/api/models/objectives.js
+++ backend/src/api/models/objectives.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('../../config/database');
 
 const Objetivo = sequelize.define("Objetivo", {
   id_objetivo: {
