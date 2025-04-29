e7e4a436da13ff544cc318b782851d9ff2bd9a12
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-04-29 11:22:43 +0200

    Redireccion a la página de login

--- frontend/reactapp/src/pages/RegistroPage.js
+++ frontend/reactapp/src/pages/RegistroPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import RegistroForm from "../components/auth/RegistroForm";
+import { Link } from "react-router-dom";
 
 function RegistroPage() {
     return (
         <div className="registro">
         <h1>Registro de usuario</h1>
         <RegistroForm />
+        <p>
+            ¿Ya tienes una cuenta? <Link to="/login">Inicia sesión</Link>
+        </p>
         </div>
     );
 }
