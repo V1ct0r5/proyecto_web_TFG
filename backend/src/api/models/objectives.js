e12e09013ba79468248bd1b7b9d3afb42cb830cc
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-01 20:56:40 +0200

    Exportación de los modelos de objetivos y usuarios como funciones

--- backend/src/api/models/objectives.js
+++ backend/src/api/models/objectives.js
@@ -1,57 +1,59 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../../config/database');
-const Usuario = require('./user');
 
-const Objetivo = sequelize.define("Objetivo", {
-  id_objetivo: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  nombre: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-    notEmpty: {
-      msg: "El nombre no puede estar vacío"
+module.exports = (sequelize, DataTypes) => {
+  const Objetivo = sequelize.define("Objetivo", {
+    id_objetivo: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "El nombre no puede estar vacío"
+        }
+      }
+    },
+    descripcion: {
+      type: DataTypes.TEXT,
+    },
+    tipo_objetivo: {
+      type: DataTypes.ENUM('Salud', 'Finanzas', 'Desarrollo personal', 'Relaciones', 'Carrera profesional', 'Otros'),
+      allowNull: false,
+    },
+    valor_cuantitativo: {
+      type: DataTypes.DECIMAL,
+    },
+    unidad_medida: {
+      type: DataTypes.STRING(50),
+    },
+    fecha_inicio: {
+      type: DataTypes.DATEONLY,
+    },
+    fecha_fin: {
+      type: DataTypes.DATEONLY,
+      validate: {
+        isAfter: {
+          args: sequelize.literal('fecha_inicio'),
+          msg: "La fecha de fin debe ser posterior a la fecha de inicio"
+        }
+      }
+    },
+    estado: {
+      type: DataTypes.ENUM('Pendiente', 'En progreso', 'Completado'),
+      defaultValue: 'Pendiente',
+      allowNull: false,
+    },
+    id_usuario: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
     }
-  },
-  descripcion: {
-    type: DataTypes.TEXT,
-  },
-  tipo_objetivo: {
-    type: DataTypes.ENUM('Salud', 'Finanzas', 'Desarrollo personal', 'Relaciones', 'Carrera profesional', 'Otros'),
-    allowNull: false,
-  },
-  valor_cuantitativo: {
-    type: DataTypes.DECIMAL,
-  },
-  unidad_medida: {
-    type: DataTypes.STRING(50),
-  },
-  fecha_inicio: {
-    type: DataTypes.DATEONLY,
-  },
-  fecha_fin: {
-    type: DataTypes.DATEONLY,
-    isAfter: {
-      args: sequelize.literal('fecha_inicio'),
-      msg: "La fecha de fin debe ser posterior a la fecha de inicio"
-    }
-  },
-  estado: {
-    type: DataTypes.ENUM('Pendiente', 'En progreso', 'Completado'),
-    defaultValue: 'Pendiente',
-  },
-  id_usuario: { // Nueva columna para la clave foránea
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Usuario, // Referencia al modelo Usuario
-      key: 'id'       // La clave primaria en el modelo Usuario
-    }
-  }
-});
+  }, {
+    tableName: 'Objetivos',
+    timestamps: true,
+  });
 
-Objetivo.belongsTo(Usuario, { foreignKey: 'id_usuario' });
-
-module.exports = Objetivo;
+  return Objetivo;
+};
--- backend/src/api/models/user.js
+++ backend/src/api/models/user.js
@@ -1,33 +1,36 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../../config/database');
-const jwt = require('jsonwebtoken');
 
-const Usuario = sequelize.define("Usuario", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  nombre_usuario: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  correo_electronico: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  contrasena: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  fecha_registro: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-});
+module.exports = (sequelize, DataTypes) => {
+  const Usuario = sequelize.define("Usuario", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombre_usuario: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    correo_electronico: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    contrasena: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    fecha_registro: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    tableName: 'Usuarios',
+    timestamps: true,
+  });
 
-module.exports = Usuario;
+  return Usuario;
+};
