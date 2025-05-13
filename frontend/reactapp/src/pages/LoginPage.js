f547f85615c22f792c20e8ba8ce53a6101be03d9
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-13 10:16:08 +0200

    Aplicación de estilos CSS a los formularios de inicio de sesión y registro

--- frontend/reactapp/src/pages/LoginPage.js
+++ frontend/reactapp/src/pages/LoginPage.js
@@ -1,17 +1,19 @@
 import React from "react";
 import LoginForm from "../components/auth/LoginForm";
 import { Link } from "react-router-dom";
+import styles from "./AuthLayout.module.css"
+
+
 
 function LoginPage() {
-  return (
-    <div className="login">
-      <h1>Iniciar Sesion</h1>
-      <LoginForm />
-      <p>
-      ¿No tienes una cuenta? <Link to="/register">Registrate</Link>
-    </p>
+  return (
+    <div className={styles.authPage}>
+        <div className={styles.formContainer}>
+            <h1 className={styles.formTitle}>Iniciar sesión</h1><LoginForm/>
+                <p className={styles.formFooter}>¿No tienes una cuenta? <Link to="/register" className={styles.formLink}>Regístrate</Link></p>
+        </div>
     </div>
-  );
+    );
 }
 
 export default LoginPage;
--- frontend/reactapp/src/pages/RegistroPage.js
+++ frontend/reactapp/src/pages/RegistroPage.js
@@ -1,17 +1,19 @@
+// frontend/reactapp/src/pages/RegistrationPage.js
 import React from "react";
-import RegistroForm from "../components/auth/RegistroForm";
+import RegistrationForm from "../components/auth/RegistroForm"; // Asegúrate de tener este componente
 import { Link } from "react-router-dom";
+import styles from "./AuthLayout.module.css"; // Asegúrate de que esta ruta sea correcta
 
-function RegistroPage() {
+function RegistrationPage() {
     return (
-        <div className="registro">
-        <h1>Registro de usuario</h1>
-        <RegistroForm />
-        <p>
-            ¿Ya tienes una cuenta? <Link to="/login">Inicia sesión</Link>
-        </p>
+        <div className={styles.authPage}>
+            <div className={styles.formContainer}>
+                <h1 className={styles.formTitle}>Crea tu cuenta</h1>
+                <RegistrationForm />
+                <p className={styles.formFooter}>¿Ya tienes una cuenta? <Link to="/login" className={styles.formLink}>Iniciar sesión</Link></p>
+            </div>
         </div>
     );
 }
 
-export default RegistroPage;
+export default RegistrationPage;
