fcd475503083c3af8ac477623b966c6ae96e7c8d
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-15 11:58:15 +0200

    Eliminación de comentarios innecesarios

--- frontend/reactapp/src/components/ui/Button.js
+++ frontend/reactapp/src/components/ui/Button.js
@@ -1,16 +1,13 @@
-// frontend/reactapp/src/components/ui/Button.js
 import React from 'react';
-import styles from './Button.module.css'; // Importar estilos específicos del botón
+import styles from './Button.module.css';
 
-// Componente de botón reutilizable
-// Acepta todas las props estándar de un botón HTML, además de children
 function Button({ children, className, ...props }) {
 
     const buttonClasses = `${styles.button} ${className || ''}`;
 
     return (
         <button className={buttonClasses} {...props}>
-        {children} {/* El texto o contenido del botón */}
+        {children}
         </button>
     );
 }
