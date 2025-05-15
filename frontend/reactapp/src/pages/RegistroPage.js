6d721367bb65337536295ef53742ac8bccf863ce
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-15 11:46:55 +0200

    Añadido div de "main-content" en el layout de registro

--- frontend/reactapp/src/pages/RegistroPage.js
+++ frontend/reactapp/src/pages/RegistroPage.js
@@ -1,16 +1,17 @@
-// frontend/reactapp/src/pages/RegistrationPage.js
 import React from "react";
-import RegistrationForm from "../components/auth/RegistroForm"; // Asegúrate de tener este componente
+import RegistrationForm from "../components/auth/RegistroForm";
 import { Link } from "react-router-dom";
-import styles from "./AuthLayout.module.css"; // Asegúrate de que esta ruta sea correcta
+import styles from "./AuthLayout.module.css";
 
 function RegistrationPage() {
     return (
         <div className={styles.authPage}>
-            <div className={styles.formContainer}>
-                <h1 className={styles.formTitle}>Crea tu cuenta</h1>
-                <RegistrationForm />
-                <p className={styles.formFooter}>¿Ya tienes una cuenta? <Link to="/login" className={styles.formLink}>Iniciar sesión</Link></p>
+            <div className={styles.mainContentArea}>
+                <div className={styles.formContainer}>
+                    <h1 className={styles.formTitle}>Crea tu cuenta</h1>
+                    <RegistrationForm />
+                    <p className={styles.formFooter}>¿Ya tienes una cuenta? <Link to="/login" className={styles.formLink}>Iniciar sesión</Link></p>
+                </div>
             </div>
         </div>
     );
