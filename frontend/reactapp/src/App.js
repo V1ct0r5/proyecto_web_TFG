3309c04ed0fd0b25ba65ae0edfc9d5c17ad48692
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-15 12:00:28 +0200

    Eliminación de comentarios innecesarios en el código

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -1,38 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from '../src/pages/LoginPage';
-import RegisterPage from '../src/pages/RegistroPage';
-import ObjectivesPage from '../src/pages/ObjetivosPage';
+import LoginPage from "../src/pages/LoginPage";
+import RegisterPage from "../src/pages/RegistroPage";
+import ObjectivesPage from "../src/pages/ObjetivosPage";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import "./styles/App.css";
 
 function App() {
-  return (
-    <Router>
-      {/* El AuthProvider envuelve toda la aplicación para proporcionar el contexto de autenticación */}
-      <AuthProvider>
-        <div className="App">
-          <Routes>
-            {/* Rutas públicas */}
+    return (
+        <Router><AuthProvider><div className="App"><Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-
-            {/* Rutas protegidas */}
-            <Route 
-              path="/objectives" 
-              element={
-                <ProtectedRoute>
-                  <ObjectivesPage />
-                </ProtectedRoute>
-              } 
+            <Route
+                path="/objectives"
+                element={
+                    <ProtectedRoute>
+                        <ObjectivesPage />
+                    </ProtectedRoute>
+                }
             />
-
             <Route path="/" element={<LoginPage />} />
-          </Routes>
-        </div>
-      </AuthProvider>
-    </Router>
-  );
+        </Routes></div></AuthProvider></Router>
+    );
 }
 
 export default App;
