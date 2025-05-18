70e18dcff576e3aeca7fda5df87b36429519c081
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-18 16:26:56 +0200

    Añadido reactToastify

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -6,22 +6,35 @@ import ObjectivesPage from "../src/pages/ObjetivosPage";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import "./styles/App.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
-        <Router><AuthProvider><div className="App"><Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route
-                path="/objectives"
-                element={
-                    <ProtectedRoute>
-                        <ObjectivesPage />
-                    </ProtectedRoute>
-                }
-            />
-            <Route path="/" element={<LoginPage />} />
-        </Routes></div></AuthProvider></Router>
+        <Router><AuthProvider><div className="App">
+            <ToastContainer
+                position="top-right" // Puedes cambiar la posición (top-left, bottom-center, etc.)
+                autoClose={3000} // Tiempo en milisegundos para que se cierren automáticamente
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            /><Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route
+                    path="/objectives"
+                    element={
+                        <ProtectedRoute>
+                            <ObjectivesPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/" element={<LoginPage />} />
+            </Routes></div></AuthProvider></Router>
     );
 }
 
