2d855a7ac19872a6af0733845664c113ee71670e
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-19 13:06:53 +0200

    Reformulación de aplicación de estilos

--- frontend/reactapp/src/pages/LoginPage.js
+++ frontend/reactapp/src/pages/LoginPage.js
@@ -1,18 +1,14 @@
 import React from "react";
 import LoginForm from "../components/auth/LoginForm";
 import { Link } from "react-router-dom";
-import styles from "./AuthLayout.module.css"
-
 
 
 function LoginPage() {
     return (
-        <div className={styles.authPage}>
-            <div className={styles.mainContentArea}>
-                <div className={styles.formContainer}>
-                    <h1 className={styles.formTitle}>Iniciar sesión</h1><LoginForm />
-                    <p className={styles.formFooter}>¿No tienes una cuenta? <Link to="/register" className={styles.formLink}>Regístrate</Link></p>
-                </div>
+        <div className="page-centered-content">
+            <div className="formContainer">
+                <h1 className="formTitle">Iniciar sesión</h1><LoginForm />
+                <p className="formFooter">¿No tienes una cuenta? <Link to="/register" className="formLink">Regístrate</Link></p>
             </div>
         </div>
     );
