082726ce5e176f60339dc76067e21f25d054c29b
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-21 15:52:07 +0200

    refactor: Mejorar la configuración de la aplicación principal

--- backend/app.js
+++ backend/app.js
@@ -6,21 +6,25 @@ const objetivosRoutes = require('./src/api/routes/objectivesRoutes');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
-const swaggerDocument = YAML.load('../docs/api/swagger.yaml');
+const errorHandler = require('./src/middlewares/errorHandler');
 
 
 const app = express();
-
 app.use(express.json());
 
 const cors = require('cors');
 app.use(cors());
 
-
+const swaggerDocument = YAML.load('../docs/api/swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.use('/api', usuariosRoutes);
 app.use('/api', objetivosRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Ruta no encontrada.' });
+});
+
+app.use(errorHandler);
 module.exports = app;
