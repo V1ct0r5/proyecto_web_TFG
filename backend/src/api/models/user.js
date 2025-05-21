94e7bcb95d5b9ae723cebac66452101005a3b3af
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-21 15:51:25 +0200

    refactor: Actualizar modelo de usuario (User) para mejorar la estructura y asociaciones

--- backend/src/api/models/user.js
+++ backend/src/api/models/user.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize) => {
   const Usuario = sequelize.define("Usuario", {
     id: {
       type: DataTypes.INTEGER,
@@ -8,12 +8,12 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
     },
     nombre_usuario: {
-      type: DataTypes.STRING(100),
+      type: DataTypes.STRING(50),
       allowNull: false,
       unique: true,
     },
     correo_electronico: {
-      type: DataTypes.STRING(100),
+      type: DataTypes.STRING(255),
       allowNull: false,
       unique: true,
       validate: {
@@ -24,14 +24,24 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING(255),
       allowNull: false,
     },
-    fecha_registro: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
   }, {
     tableName: 'Usuarios',
     timestamps: true,
+    underscored: true,
   });
+
+  Usuario.associate = (models) => {
+    Usuario.hasMany(models.Objective, {
+        foreignKey: 'id_usuario', // Clave foránea en la tabla 'objectives'
+        as: 'objetivos',          // Alias para incluir objetivos con el usuario
+        onDelete: 'CASCADE'       // Eliminar objetivos del usuario si el usuario es eliminado
+    });
+    Usuario.hasMany(models.Progress, {
+        foreignKey: 'id_usuario', // Clave foránea en la tabla 'progress'
+        as: 'progresos',          // Alias para incluir progresos con el usuario
+        onDelete: 'CASCADE'       // Eliminar progresos del usuario si el usuario es eliminado
+    });
+  };
 
   return Usuario;
 };
