0337aea9508060f457b54e829b5fa933b7f327b1
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-21 15:50:21 +0200

    refactor: Actualizar nombres de middlewares de validación en rutas de usuario

--- backend/src/api/routes/userRoutes.js
+++ backend/src/api/routes/userRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/userController');
-const { validarCrearUsuario, validarActualizarUsuario, validarLoginUsuario } = require('../../middlewares/userValidation');
+const { validarRegistroUsuario, validarInicioSesion, validarActualizacionUsuario } = require('../../middlewares/userValidation');
 const authMiddleware = require('../../middlewares/authMiddleware');
 
-router.post('/auth/register', validarCrearUsuario, usuariosController.registrarUsuario);
-router.post('/auth/login', validarLoginUsuario, usuariosController.iniciarSesionUsuario);
+router.post('/auth/register', validarRegistroUsuario, usuariosController.registrarUsuario);
+router.post('/auth/login', validarInicioSesion, usuariosController.iniciarSesionUsuario);
 router.delete('/auth/logout', authMiddleware, usuariosController.cerrarSesionUsuario);
 
 
 router.get('/users', authMiddleware, usuariosController.obtenerUsuarios);
-router.post('/users', authMiddleware, validarCrearUsuario, usuariosController.crearUsuario);
+router.post('/users', authMiddleware, validarRegistroUsuario, usuariosController.crearUsuario);
 router.get('/users/:id', authMiddleware, usuariosController.obtenerUsuarioPorId);
-router.put('/users/:id', authMiddleware, validarActualizarUsuario, usuariosController.actualizarUsuario);
+router.put('/users/:id', authMiddleware, validarActualizacionUsuario, usuariosController.actualizarUsuario);
 router.delete('/users/:id', authMiddleware, usuariosController.eliminarUsuario);
 
 
