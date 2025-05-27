a5d6d99f9c8f615a455758d6114fc8eef9354d7e
VíctorMorenoBueno <victor.moreno@alumnos.upm.es>
2025-05-27 11:52:54 +0200

    feat(AppError): Añadir soporte para 'errorsData' en errores personalizados
    Mejorar la clase AppError para permitir la inclusión opcional de
    datos de error estructurados ('errorsData') a través del constructor,
    facilitando un manejo de errores más detallado.

--- backend/src/utils/AppError.js
+++ backend/src/utils/AppError.js
@@ -2,20 +2,26 @@
  * @class AppError
  * @extends Error
  * @description Clase personalizada para manejar errores operacionales de la aplicación.
- * Permite especificar un código de estado HTTP y un indicador de si el error es operacional.
+ * Permite especificar un código de estado HTTP, un indicador de si el error es operacional,
+ * y opcionalmente, un array de datos de error (ej. para errores de validación).
  */
 class AppError extends Error {
     /**
      * Constructor para AppError.
      * @param {string} message - Mensaje de error.
      * @param {number} statusCode - Código de estado HTTP.
+     * @param {Array<Object>|Object} [errorsData] - Datos adicionales del error, como un array de errores de validación.
      */
-    constructor(message, statusCode) {
+    constructor(message, statusCode, errorsData = undefined) {
         super(message);
 
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = true; // Para distinguir errores de programación de errores operacionales
+        
+        if (errorsData) {
+            this.errorsData = errorsData; // Almacenar datos de error adicionales
+        }
 
         Error.captureStackTrace(this, this.constructor);
     }
